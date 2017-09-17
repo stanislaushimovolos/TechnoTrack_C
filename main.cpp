9d1599830ebaeea0dff23b0c1b83018f582afce1
stanislaushimovolos <super.stas.stanislaw@gmail.com>
2017-09-18 02:17:34 +0300

    Added comments & improved code style

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 
 void swapCharPointers (char **a, char **b);
 
-void alphabetSorter (char **text, const int numOfStrings);
+void sortByAlphabet (char **text, const int numOfStrings);
 
-size_t stringsCounter (const char *buffer, size_t sizeOfBuffer);
+size_t countStrings (const char *buffer, size_t sizeOfBuffer);
 
-void textCreator (char **text, char *buffer, const int sizeOfBuffer);
+void createLinesPointers (char **text, char *buffer, const int sizeOfBuffer);
 
 void writeSorted (char **text, size_t numOfStrings, int userChoice);
 
@@ -17,33 +17,45 @@ const int writeFile = 2;
 
 
 int main () {
-	int userChoice = 0;
 
+	// Load file
 	FILE *file = fopen ("poem.txt", "r");
 
 	if (file == NULL) {
 		printf ("FILE ERROR");
 		return 0;
 	}
+
+	// Get buffer size
+
 	fseek (file, 0, SEEK_END);
-
 	size_t sizeOfBuffer = (size_t) ftell (file);
-	char *buffer = (char *) calloc (sizeOfBuffer, sizeof (char));
-
 	fseek (file, 0, SEEK_SET);
+
+	// Create buffer and load file
+
+	char *buffer = (char *) calloc (sizeOfBuffer, sizeof (char));
 	fread (buffer, 1, sizeOfBuffer, file);
 	fclose (file);
 
-	size_t numOfStrings = stringsCounter (buffer, sizeOfBuffer);
+	// Count lines and create array of lines pointers
+
+	size_t numOfStrings = countStrings (buffer, sizeOfBuffer);
 	char **text = (char **) calloc (numOfStrings, sizeof (char *));
+	createLinesPointers (text, buffer, sizeOfBuffer);
 
-	textCreator (text, buffer, sizeOfBuffer);
-	alphabetSorter (text, numOfStrings);
+	// Sort lines
 
+	sortByAlphabet (text, numOfStrings);
+
+	// Print or write sorted lines
+
+	int userChoice = 0;
 	printf ("Poem_Sorter \nDisplay(%d) or write to the file(%d)? ", writeDisplay, writeFile);
 	scanf ("%d", &userChoice);
-
 	writeSorted (text, numOfStrings, userChoice);
+
+	// Finish
 
 	free (text);
 	free (buffer);
@@ -58,7 +70,7 @@ void swapCharPointers (char **a, char **b) {
 }
 
 
-void alphabetSorter (char **text, const int numOfStrings) {
+void sortByAlphabet (char **text, const int numOfStrings) {
 	for (int i = 0; i < numOfStrings - 1; i++)
 		for (int j = 0; j < numOfStrings - i - 1; j++)
 			if (strcasecmp (text[j], text[j + 1]) > 0)
@@ -66,7 +78,7 @@ void alphabetSorter (char **text, const int numOfStrings) {
 }
 
 
-size_t stringsCounter (const char *buffer, size_t sizeOfBuffer) {
+size_t countStrings (const char *buffer, size_t sizeOfBuffer) {
 	size_t numberOfStrings = 1;
 	for (int i = 0; i < sizeOfBuffer; i++)
 		if (buffer[i] == '\n')
@@ -75,7 +87,7 @@ size_t stringsCounter (const char *buffer, size_t sizeOfBuffer) {
 }
 
 
-void textCreator (char **text, char *buffer, const int sizeOfBuffer) {
+void createLinesPointers (char **text, char *buffer, const int sizeOfBuffer) {
 	text[0] = buffer;
 	for (int i = 0, j = 1; i < sizeOfBuffer; i++)
 		if (buffer[i] == '\n' || buffer[i] == '\r') {
@@ -87,13 +99,13 @@ void textCreator (char **text, char *buffer, const int sizeOfBuffer) {
 
 void writeSorted (char **text, size_t numOfStrings, int userChoice) {
 	switch (userChoice) {
-		case 1:
+		case writeDisplay:
 			for (int i = 0; i < numOfStrings; i++)
 				if (text[i][0] != 0)
 					printf ("%s\n", text[i]);
 			break;
 
-		case 2: {
+		case writeFile: {
 			FILE *file2 = fopen ("sorted_poem.txt", "w");
 			for (int i = 0; i < numOfStrings; i++)
 				if (text[i][0] != 0)
